f7a8421d727d8f9613460481f22b3e1d67b4319a
crazyms <crazyms520@hotmail.com>
2015-10-21 23:57:56 +0800

    Revert "註冊＆登出"
    This reverts commit ec4cc13a69bc369d6df83adc41bcc0979aa37b7a.

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -9,27 +9,23 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$login = $this->session->userdata('login');
-		$user_id = $this->session->userdata('user_id');
-		$user = $this->user->get_user_by_id($user_id);
 		$navbar = $this->load->view('_navbar',array(
 			'page' => 'home',
 			'login' => $login
 			),true);
 		$this->load->view('welcome_message',array(
-			'navbar' => $navbar,
-			'user' => $user
+			'navbar' => $navbar
 			));
 	}
 
 	public function login(){
-		$account = $this->input->post('account');
+		$email = $this->input->post('email');
 		$password = $this->input->post('password');
-		$user = $this->user->get_user_by_acc_pas($account,$password);
-		if($account && $password){
+		$user = $this->user->get_user_by_mail_pas($email,$password);
+		if($email && $password){
 			if($user){
 				$this->session->set_userdata('user_id',$user->id);
 				$this->session->set_userdata('login','YES');
-				$this->session->set_flashdata('message','登入成功');
 				redirect('welcome');
 			}else{
 				$this->session->set_flashdata('message','登入失敗');
@@ -40,49 +36,7 @@ class Welcome extends CI_Controller {
 			redirect('welcome');
 		}
 	}
-
-	public function register(){
-		$login = $this->session->userdata('login');
-		$navbar = $this->load->view('_navbar',array(
-			'login' => $login,
-			'page' => ''
-			),true);
-		$this->load->view('register',array(
-			'navbar' => $navbar
-			));
-	}
-
-	public function register_post(){
-		$account = $this->input->post('account');
-		$name = $this->input->post('name');
-		$password = $this->input->post('password');
-		$repassword = $this->input->post('repassword');
-		$user = $this->user->get_user_by_acc_pas($account,$password);
-		if(!$user){
-			if($password == $repassword){
-				$id = $this->user->register(array(
-					'account' => $account,
-					'name' => $name,
-					'password' => $password
-					));
-			}else{
-				$this->session->set_flashdata('message','密碼確認錯誤');
-				redirect('welcome/register');
-			}
-		}else{
-			$this->session->set_flashdata('message','帳號重覆');
-			redirect('welcome/register');
-		}
-		if($id){
-			$this->session->set_flashdata('message','註冊成功快登入吧');
-			return redirect(site_url('welcome'));
-		}
-		$this->load->view('register');
-	}
-
-	public function logout(){
-		$this->session->unset_userdata('user_id');
-		$this->session->unset_userdata('login');
-		return redirect(site_url('welcome'));
-	}
 }
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -21,43 +21,28 @@
 </head>
 <body>
 	<?php echo $navbar ; ?>
-
-
+	<?php if($message = $this->session->flashdata('message')){
+			echo $message;
+		}
+	?>
 	<div class='container'>
-		<?php if($message = $this->session->flashdata('message')){?>
-			<div class='alert alert-info'><?php echo $message; ?></div>
-		<?php } ?>
 		<div class="row">
 		  <div class="col-md-4"></div>
 		  <div class="col-md-4">
-		  	<?php if($login){ ?>
-		  		<div class="page-header">
-					  <h1>首頁<small>購物車練習</small></h1>
-					</div>
-					<div class='text-left'>
-						<?php echo 'Hi！'.$user->name.' 歡迎回來'; ?>
-					</div>
-		  	<?php }else{ ?>
-		  		<div class="page-header">
-					  <h1>首頁<small>購物車練習</small></h1>
-					</div>
-					<div class='text-left'>
-					</div>
 		  	<form role="form" action='<?php echo site_url('welcome/login'); ?>' method='post'>
 				  <div class="form-group">
-				    <label for="account">帳號</label>
-				    <input type="text" class="form-control" name='account' id="account" placeholder="輸入帳號">
+				    <label for="Email">電子郵件</label>
+				    <input type="text" class="form-control" name='email' id="email" placeholder="輸入電子郵件">
 				  </div>
 				  <div class="form-group">
 				    <label for="Password">密碼</label>
-				    <input type="password" class="form-control" name='password' id="password" placeholder="輸入密碼">
+				    <input type="password" class="form-control" name='password' id="password" placeholder="Password">
 				  </div>
 				  <div class="form-group text-right">
 				  	<button type="submit" class="btn btn-default">送出</button>
 				  </div>
 				</form>
 		  </div>
-		  <?php } ?>
 		  <div class="col-md-4"></div>
 		</div>
 	</div>
